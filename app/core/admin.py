3e38ef7176ef5a26dd541c90dc360a3153d65042
Shailendra <kumarshalendra1702@gmail.com>
2021-10-16 18:34:51 +0530

    configured postgres db.

--- app/core/admin.py
+++ app/core/admin.py
@@ -18,9 +18,9 @@ class UserAdmin(BaseUserAdmin):
         (_('Important dates'), {'fields': ('last_login',)})
     )
     add_fieldsets = (
-        (None,{
-            'classes':('wide',),
-            'fields':('email','password1','password2')
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
         }),
     )
 
