b5f30d24347063228fba911a67ba3dcef148d79c
Shailendra <kumarshalendra1702@gmail.com>
2021-10-24 14:58:03 +0530

    Added create tag feature

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -34,7 +34,7 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
-    
+
     def test_create_user_page(self):
         """Tst that the create user page works"""
         url = reverse('admin:core_user_add')
